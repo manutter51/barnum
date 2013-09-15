d9fb41cbc37dbbc3e8ff913c478fa6ce8edd1e7a
Mark Nutter <manutter51@gmail.com>
2013-09-15 10:53:25 -0400

    Added with-events macro

--- src/barnum/events.clj
+++ src/barnum/events.clj
@@ -17,14 +17,20 @@ whose name matches the regex. For any given key, the handlers will be called in 
 they were defined. Handler functions take two args. The first will be a response map, containing
 the response of the previous handler, initially {}. The second will be a vector of any additional
 arguments given when the event is triggered"}
-  register-handler [event-map event-key handler-fn]
+  register-handler* [event-map event-key handler-fn]
   (r/register-handler event-map event-key handler-fn))
+
+(defn ^{:doc "Removes the given handler from the given event key(s). The event key can be a
+single keyword, or a set of keywords, to match any of the contained keys, or a regex pattern,
+to match any key whose name matches the regex."}
+  remove-handler* [event-map event-key handler-fn]
+  (r/remove-handler event-map event-key handler-fn))
 
 (defn
   ^{:doc "Triggers the event corresponding to the given key, which must be a single keyword.
 Any additional arguments will be passed to each of the registered handlers. The trigger
 function returns immediately; event processing is handled in a different thread."}
-  trigger [event-map event-key & args]
+  trigger* [event-map event-key & args]
   ;; TODO implement this method
   nil
   )
@@ -34,7 +40,7 @@ function returns immediately; event processing is handled in a different thread.
 Any additional arguments will be passed to each of the registered handlers. The poll
 function does not return until all handlers have been called on the function, and returns
 the result of the last handler that was called."}
-  poll [event-map event-key & args]
+  poll* [event-map event-key & args]
   ;; TODO implement this method
   nil
   )
@@ -42,7 +48,7 @@ the result of the last handler that was called."}
 (defn
   ^{:doc "Compiles the event dictionary for faster processing. Does nothing if the
 dictionary has already been compiled."}
-  compile [event-map]
+  compile* [event-map]
   ;; TODO implement this method
   nil
   )
@@ -50,6 +56,28 @@ dictionary has already been compiled."}
 (defn
   ^{:doc "Returns the help string that was provided when the event was declared."
     }
-  help [event-dictionary event-key]
-  (let [help (:help (meta event-dictionary))]
+  help* [event-map event-key]
+  (let [help (:help (meta event-map))]
     (or (event-key help) (str "Unknown event key: " event-key))))
+
+(def ^:dynamic *event-map* (atom nil))
+
+(defn
+  ^{:doc "Registers a handler for the given event or set of events."}
+  register-handler [event-key handler-fn]
+  (swap! *event-map* register-handler* event-key handler-fn)
+  handler-fn)
+
+(defn ^{:doc "Triggers an event asynchronously."}
+  trigger [event-key & args]
+  (apply trigger* @*event-map* event-key args))
+
+(defn ^{:doc "Returns the result of polling all the event handlers for the given event key"}
+  poll [event-key & args]
+  (apply poll* @*event-map* event-key args))
+
+(defmacro with-events [event-map & body]
+  `(binding [*event-map* (atom (barnum.events/declare ~event-map))]
+     ~@body))
+
+
--- src/barnum/events/register.clj
+++ src/barnum/events/register.clj
@@ -2,3 +2,6 @@
 
 (defn register-handler [event-map event-key handler-fn]
   nil)
+
+(defn remove-handler [event-map event-key handler-fn]
+  nil)
