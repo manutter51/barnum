34dc0585be198be244913f9acb19a479809a1558
Mark Nutter <manutter51@gmail.com>
2013-09-16 06:09:23 -0400

    add-handler-to-key

--- src/barnum/events/register.clj
+++ src/barnum/events/register.clj
@@ -1,5 +1,10 @@
 (ns barnum.events.register)
 
+(defn- add-handler-to-key [event-map event-key handler-fn]
+  (if-let [current (event-key event-map)]
+    (assoc event-map event-key (conj current handler-fn))
+    (throw (Exception. (str "Unknown event key " (name event-key))))))
+
 (defn register-handler [event-map event-key handler-fn]
   nil)
 
