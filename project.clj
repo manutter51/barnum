fe586c6f1b24f1894b50735079f60bb773a836ac
Mark Nutter <manutter51@gmail.com>
2013-11-08 07:34:50 -0500

    Update project.clj for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject barnum "0.1.0-SNAPSHOT"
+(defproject barnum "0.1.0"
   :description "A simple event lib: define your own events, register
 handlers for them, and fire them."
   :url "http://github.com/manutter51/barnum"
