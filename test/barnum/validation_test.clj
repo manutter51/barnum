c41e97af6b753340b3d4052cf00b239f6c4decf8
Mark Nutter <manutter51@gmail.com>
2013-11-18 07:43:28 -0500

    Fix validation tests to use new API

--- test/barnum/validation_test.clj
+++ test/barnum/validation_test.clj
@@ -19,7 +19,7 @@
                    {:validation-fn require-all}
                    :param1 :param2)
         (add-handler :e1 :h1 do-nothing-handler)
-        @(fire :e1 :param1 :foo)
+        (fire :e1 :param1 :foo)
         => (exactly [:fail '("Required value missing for key :param2")]))
   
   (fact "The require-all function passes when all params are present with non-nil values"
@@ -28,7 +28,7 @@
                    {:validation-fn require-all}
                    :param1 :param2)
         (add-handler :e1 :h1 do-nothing-handler)
-        @(fire :e1 :param1 :foo :param2 :bar)
+        (fire :e1 :param1 :foo :param2 :bar)
         => (exactly '([:ok nil])))
 
   (fact "The restrict-all function fails if any keys in the args are not present in the params list."
@@ -37,7 +37,7 @@
                    {:validation-fn restrict-all}
                    :param1)
         (add-handler :e1 :h1 do-nothing-handler)
-        @(fire :e1 :param1 :foo :param2 :bar)
+        (fire :e1 :param1 :foo :param2 :bar)
         => (exactly [:fail '("Unexpected key :param2 in arg list")]))
 
   (fact "The restrict-all function passes if all keys in the args are present in the params list."
@@ -46,7 +46,7 @@
                    {:validation-fn restrict-all}
                    :param1 :param2)
         (add-handler :e1 :h1 do-nothing-handler)
-        @(fire :e1 :param1 :foo :param2 :bar)
+        (fire :e1 :param1 :foo :param2 :bar)
         => (exactly '([:ok nil])))
 
   (fact "The restrict-all function passes if some keys in the params list are not present in the args list"
@@ -55,6 +55,6 @@
                    {:validation-fn restrict-all}
                    :param1 :param2 :param3)
         (add-handler :e1 :h1 do-nothing-handler)
-        @(fire :e1 :param1 :foo :param2 :bar)
+        (fire :e1 :param1 :foo :param2 :bar)
         => (exactly '([:ok nil])))
 )
